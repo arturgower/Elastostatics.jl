83b08b80e3f0d26b9216f66594b6797324460a09
matheusloures42 <matheusloures45@gmail.com>
2025-11-04 20:34:11 +0000

    grammar correction

--- src/types.jl
+++ src/types.jl
@@ -93,7 +93,7 @@ end
 field(fr::FieldResults) = fr.field
 
 struct FundamentalSolution{T<:Real,Dim,FieldDim}
-    x_source::Vector{SVector{Dim,T}}
+    source_positions::Vector{SVector{Dim,T}}
     coefficients:: Vector{SVector{FieldDim,T}}
 end
 
