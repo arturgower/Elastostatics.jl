b5ed943e396b1c982d4fa1b1a1edb1e5e5077f0f
arturgower <arturgower@gmail.com>
2025-11-09 12:21:47 +0000

    add option offset_percent to interior_points_along_coordinate

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,15 @@
-function interior_points_along_coordinate(points; coordinate_number = 2, segments = 10)
+function interior_points_along_coordinate(points; offset_percent = 0.1,
+    coordinate_number = 2, segments = 10)
     
     cs = [xy[coordinate_number] for xy in points]
 
+    meanc = mean(cs)
     minc = minimum(cs)
+    minc = minc + offset_percent * (meanc - minc)
+
     maxc = maximum(cs)
+    maxc = maxc + offset_percent * (meanc - maxc)
+
     edges = collect(range(minc, stop=maxc, length=segments+1))
 
     # collect indices for each y-bin
