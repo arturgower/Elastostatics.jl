ec00306ec65561ab55e0d9bafe2681158fe7898d
Artur Gower <arturgower@gmail.com>
2025-11-18 14:33:23 +0000

    correct interior_points_along_coordinate

--- src/utils.jl
+++ src/utils.jl
@@ -1,14 +1,14 @@
 function interior_points_along_coordinate(points; offset_percent = 0.1,
     coordinate_number = 2, segments = 10)
+
+    if offset_percent < 0.0 || offset_percent > 1.0
+        throw(ArgumentError("offset_percent must be between 0.0 and 1.0"))
+    end
     
     cs = [xy[coordinate_number] for xy in points]
 
-    meanc = mean(cs)
     minc = minimum(cs)
-    minc = minc + offset_percent * (meanc - minc)
-
     maxc = maximum(cs)
-    maxc = maxc + offset_percent * (meanc - maxc)
 
     edges = collect(range(minc, stop=maxc, length=segments+1))
 
@@ -23,7 +23,14 @@ function interior_points_along_coordinate(points; offset_percent = 0.1,
     bin_indices = filter(v -> !isempty(v), bin_indices)
 
     interior_points = map(bin_indices) do inds
-        mean(points[inds])
+        p = mean(points[inds])
+        
+        inner_low = minc + offset_percent * (maxc - minc)
+        inner_high = maxc - offset_percent * (maxc - minc)
+        pc = p[coordinate_number]
+        t = clamp((pc - minc) / (maxc - minc), 0.0, 1.0)
+        p[coordinate_number] = inner_low + t * (inner_high - inner_low)
+        p
     end
 
     return interior_points
